3dcae250c7d986d91d48a2a8e46c50b9714d9b62
MrAleksby <alekskosmos999@gmail.com>
2026-05-05 09:55:50 +0500

    Revert to default Firestore with IndexedDB cache

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { initializeFirestore, memoryLocalCache } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -12,5 +12,5 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const db = initializeFirestore(app, { localCache: memoryLocalCache() })
+export const db = getFirestore(app)
 export const auth = getAuth(app)
